aafa6aa8756fc3a291e5e9af66c8cd3a6ce4a488
Neemias <167483565+neemo-o@users.noreply.github.com>
2024-09-16 16:59:31 -0300

    Add files via upload

---
+++ Sistema/app.py
@@ -0,0 +1,89 @@
+import json
+import openpyxl
+import requests
+from datetime import datetime
+
+# Leitura das informações da Planilha
+planilha = openpyxl.load_workbook("./dados/Banco de Dados.xlsx")
+paginaPlanilha = planilha["Dados"]
+
+# Leitura das infomações do BOT
+with open('./dados/config.json', 'r') as f:
+    infos = json.load(f)
+
+# Envio de mensagens:
+def enviarMensagem(numero, cliente, data, preco, conteudo):
+    image_url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhightouch.com%2Fblog%2Ffacebook-capi&psig=AOvVaw0Sc11rYxvnk56lEURSKrsv&ust=1725800395749000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiz3NDxsIgDFQAAAAAdAAAAABAE"
+    download_link = "https://drive.google.com/uc?export=download&id=1AO8YR8ahBP8oB0-CrHALRt-8nP2CPRES"
+
+    url = f"https://graph.facebook.com/{infos['VERSION']}/{infos['PHONE_NUMBER_ID']}/messages"
+    headers = {
+        "Authorization": "Bearer " + infos['ACCESS_TOKEN'],
+        "Content-Type": "application/json",
+    }
+
+    if conteudo == None:
+        conteudo = ""
+
+    data = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": f"+55{numero}",
+        "type": "template",
+        "template": {"name": "cobranca", "language": {"code": "pt_BR"}, 
+                     "components": [ 
+                         {
+                            "type": "header",
+                            "parameters": [
+                            {"type": "document", "document": {"link": download_link, "filename": "qrcode.pdf"}}
+                        ]   
+                         },
+                         {"type": "body", 
+                          "parameters": [
+                            {"type": "text", "text": f"{cliente}"},
+                            {"type": "text", "text": f"{data}"},   
+                            {"type": "text", "text": f"{preco}"},
+                            {"type": "text", "text": f"> Mensalidade" + f"{conteudo}"}  
+                            ] 
+                         },
+                         {
+                            "type": "button",
+                            "sub_type": "quick_reply",
+                            "index": "0",
+                            "parameters": [
+                                {
+                                    "type": "payload",
+                                    "payload": "chave_pix"  # URL do botão personalizado
+                                }
+                            ]
+                         },
+                         {
+                            "type": "button",
+                            "sub_type": "quick_reply",
+                            "index": "1",
+                            "parameters": [
+                                {
+                                    "type": "payload",
+                                    "payload": "duvidas"  
+                                }
+                            ]
+                         }
+                       ]
+                     }
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    return response
+
+
+# Enviar para os clientes:
+for linha in paginaPlanilha.iter_rows(min_row=2, max_row=2):
+    # clientes, número, data
+    cliente = linha[0].value
+    numero = linha[1].value
+    data = linha[2].value
+    data_formatada = data.strftime('%d/%m/%Y')
+    preco = linha[3].value
+    conteudo = linha[4].value
+    response = enviarMensagem(numero, cliente, data_formatada, preco, conteudo)
+    print(cliente, numero, data_formatada, preco, conteudo, f"Response status: {response.status_code}")
